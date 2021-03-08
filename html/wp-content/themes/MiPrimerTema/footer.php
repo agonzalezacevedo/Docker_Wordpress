1f1c4f20617e92b801cb8efad443eafbb04068ec
agonzalezacevedo <agonzalezacevedo@danielcastelao.org>
2021-03-08 15:52:07 +0100

    Final

---
+++ html/wp-content/themes/MiPrimerTema/footer.php
@@ -0,0 +1,9 @@
+<?php
+echo "<h1> FOOTERRRRRRRRRRRRRRRR </h1>";
+?>
+<div id="header">
+    <div class="description">
+            <img src="<?php echo get_stylesheet_directory_uri();?>/images/noIdea.jpg" >
+    </div>
+</div>
+<?php echo "<h2 style='color: #09ec8a'>Final del footer</h2>";?>
---
+++ html/wp-content/themes/MiPrimerTema/header.php
@@ -0,0 +1,16 @@
+<?php
+echo "<h1> HEADERRRRRRRRRRRRRRRR </h1>";
+?>
+<div id="header">
+    <div id="headerimg">
+        <h1>
+            <a href="<?php echo get_option('home'); ?>">
+                <?php bloginfo('name'); ?></a>
+        </h1>
+        <div class="description">
+            <?php bloginfo('description'); ?>
+            <br>
+            <img src="<?php echo get_stylesheet_directory_uri();?>/images/perritoSovietico.jpg" >
+        </div>
+    </div>
+</div>
